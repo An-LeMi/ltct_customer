7f5b0ff7ed3ff20d1b64b5c9d28ad9dd123cf495
Lê Minh An <leminhan18@gmail.com>
2021-12-20 11:20:51 +0700

    Add phone

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -41,7 +41,7 @@ class AuthController extends Controller
             'username' => 'required|unique:users',
             'password' => 'required|confirmed',
             'email' => 'required|email|unique:users',
-            'phone' => ['required', 'regex:/^([0-9\s\-\+\(\)]*)$/', 'digits_between:10,12', Rule::unique('users')->ignore($request->id)],
+            'phone' => 'required|unique:users|digits:10',
         ]);
 
         // check user exist with phone
